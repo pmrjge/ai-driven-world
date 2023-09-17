97627e7bc086d8c8856f50540ff82b947913efbc
Pedro Jorge <2230393@my.ipleiria.pt>
2023-09-17 22:21:12 +0100

    Following the book defined the XOR Dataset as an example to be studied

--- src/impl_nn_flax.py
+++ src/impl_nn_flax.py
@@ -1,6 +1,9 @@
 import flax
+import numpy as np
 from flax import linen as nn
 import jax
+import torch.utils.data as data
+import matplotlib.pyplot as plt
 
 
 class SimpleClassifier(nn.Module):
@@ -40,3 +43,55 @@ rng, inp_rng, init_rng = jax.random.split(rng, 3)
 inp = jax.random.normal(inp_rng, (8, 2))
 params = model.init(init_rng, inp)
 print(params)
+
+# Define Dataset for training
+
+
+class XORDataset(data.Dataset):
+    def __init__(self, size, seed, std=0.1):
+        super().__init__()
+        self.size = size
+        self.np_rng = np.random.RandomState(seed=seed)
+        self.std = std
+        self.generate_continuous_xor()
+
+    def generate_continuous_xor(self):
+        data = self.np_rng.randint(low=0, high=2, size=(self.size, 2)).astype(
+            np.float32
+        )
+        label = (data.sum(axis=1) == 1).astype(np.int32)
+
+        data += self.np_rng.normal(loc=0.0, scale=self.std, size=data.shape)
+
+        self.data = data
+        self.label = label
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        data_point = self.data[idx]
+        data_label = self.label[idx]
+        return data_point, data_label
+
+
+dataset = XORDataset(size=200, seed=4)
+print("Size of dataset:", len(dataset))
+print("Data point 0:", dataset[0])
+
+
+def visualize_samples(data, label):
+    data_0 = data[label == 0]
+    data_1 = data[label == 1]
+
+    plt.figure(figsize=(4, 4))
+    plt.scatter(data_0[:, 0], data_0[:, 1], edgecolor="#333", label="Class 0")
+    plt.scatter(data_1[:, 0], data_1[:, 1], edgecolor="#333", label="Class 1")
+    plt.title("Dataset samples")
+    plt.ylabel(r"$x_2$")
+    plt.xlabel(r"$x_1$")
+    plt.legend()
+
+
+visualize_samples(dataset.data, dataset.label)
+plt.show()
