f5513669aa5ab415173c1917657c7390e9c2f689
Pedro Jorge <2230393@my.ipleiria.pt>
2023-09-17 14:54:48 +0100

    Examples for compile time JIT functions

--- src/jaxpr_medium.py
+++ src/jaxpr_medium.py
@@ -2,6 +2,7 @@ import jax
 import jax.numpy as jnp
 import jax.random as jr
 
+from timeit import default_timer as time
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -38,3 +39,29 @@ val_grad_fn = jax.value_and_grad(simple_graph)
 print(val_grad_fn(inp))
 
 # Speeding up with jit
+jitted_fn = jax.jit(simple_graph)
+
+key = jr.PRNGKey(42)
+rng, normal_rng = jr.split(key)
+
+large_input = jr.normal(normal_rng, (1000,))
+_ = jitted_fn(large_input)
+
+
+def performance(f, x, s="Default"):
+    t0 = time()
+    f(x).block_until_ready()
+    t1 = time()
+    print(f"{s}: Time elapsed {t1 - t0} ms")
+
+
+performance(simple_graph, large_input, "simple graph")
+performance(jitted_fn, large_input, "jitted")
+
+# Applying/ Compiling JIT for gradient functions
+
+jitted_grad_fn = jax.jit(grad_fn)
+_ = jitted_grad_fn(large_input)
+
+performance(grad_fn, large_input, "grad_fn")
+performance(jitted_grad_fn, large_input, "jitted_grad_fn")
