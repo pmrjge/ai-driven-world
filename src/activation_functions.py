3319c317dac904fe59660fdfbb68523b4e0543a5
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-12 19:43:24 +0100

    Downloaded models to work out our understanding of activation functions

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -21,3 +21,36 @@ from flax.training import train_state, checkpoints
 
 import optax
 
+import urllib.request
+from urllib.error import HTTPError
+
+DATASET_PATH = "../../data"
+CHECKPOINT_PATH = "../../saved_models/tutorial3_jax"
+
+
+base_url = "https://raw.githubusercontent.com/phlippe/saved_models/main/JAX/tutorial3/"
+
+pretrained_files = ["FashionMNIST_elu.config", "FashionMNIST_elu.tar",
+                    "FashionMNIST_leakyrelu.config", "FashionMNIST_leakyrelu.tar",
+                    "FashionMNIST_relu.config", "FashionMNIST_relu.tar",
+                    "FashionMNIST_sigmoid.config", "FashionMNIST_sigmoid.tar",
+                    "FashionMNIST_swish.config", "FashionMNIST_swish.tar",
+                    "FashionMNIST_tanh.config", "FashionMNIST_tanh.tar"]
+
+
+# Create checkpoint path if it doesn't exist yet
+os.makedirs(CHECKPOINT_PATH, exist_ok=True)
+
+# For each file, check whether it already exists. If not, try downloading it.
+for file_name in pretrained_files:
+    file_path = os.path.join(CHECKPOINT_PATH, file_name)
+    if not os.path.isfile(file_path):
+        file_url = base_url + file_name
+        print(f"Downloading {file_url}...")
+        try:
+            urllib.request.urlretrieve(file_url, file_path)
+        except HTTPError as e:
+            print("Something went wrong. Please contact the author with the full output including the following error:\n", e)
+
+
+# Common activation functions
