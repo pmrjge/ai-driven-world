d66e87bb79074cecb713e5e8d0b5d889eb57d375
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-12 11:44:56 +0100

    Finished the model evaluation and displayed boundaries for classification

--- src/impl_nn_flax.py
+++ src/impl_nn_flax.py
@@ -173,3 +173,57 @@ checkpoints.save_checkpoint(ckpt_dir=ckpt_dir, target=trained_model_state, step=
 loaded_model_state = checkpoints.restore_checkpoint(ckpt_dir=ckpt_dir, target=model_state, prefix=prefix)
 
 # Evaluation
+
+test_dataset = XORDataset(size=512, seed=123)
+
+test_data_loader = data.DataLoader(test_dataset, batch_size=128, shuffle=False, drop_last=False, collate_fn=numpy_collate)
+
+def eval_model(state, data_loader):
+    all_accs, batch_sizes = [], []
+    for batch in data_loader:
+        batch_acc = eval_step(state, batch)
+        all_accs.append(batch_acc)
+        batch_sizes.append(batch[0].shape[0])
+
+    acc = sum([a * b for a,b in zip(all_accs, batch_sizes)]) / sum(batch_sizes)
+    print(f"Accuracy of the model: {100.0 * acc:4.2f}%")
+
+eval_model(trained_model_state, test_data_loader)
+
+# binding model parameters
+
+trained_model = model.bind(trained_model_state.params)
+
+data_input, labels = next(iter(data_loader))
+out = trained_model(data_input)
+print(out.shape)
+
+def visualize_classification(model, data, label):
+    data_0 = data[label == 0]
+    data_1 = data[label == 1]
+
+    fig = plt.figure(figsize=(4,4), dpi=500)
+    plt.scatter(data_0[:,0], data_0[:,1], edgecolor="#333", label="Class 0")
+    plt.scatter(data_1[:,0], data_1[:,1], edgecolor="#333", label="Class 1")
+    plt.title("Dataset samples")
+    plt.ylabel(r"$x_2$")
+    plt.xlabel(r"$x_1$")
+    plt.legend()
+
+    # Let's make use of a lot of operations we have learned above
+    c0 = np.array((1, 0, 0, 0.5))
+    c1 = np.array((0, 0, 1, 0.5))
+    x1 = jnp.arange(-0.5, 1.5, step=0.01)
+    x2 = jnp.arange(-0.5, 1.5, step=0.01)
+    xx1, xx2 = jnp.meshgrid(x1, x2, indexing='ij')  # Meshgrid function as in numpy
+    model_inputs = np.stack([xx1, xx2], axis=-1)
+    logits = model(model_inputs)
+    preds = nn.sigmoid(logits)
+    output_image = (1 - preds) * c0[None,None] + preds * c1[None,None]  # Specifying "None" in a dimension creates a new one
+    output_image = jax.device_get(output_image)  # Convert to numpy array. This only works for tensors on CPU, hence first push to CPU
+    plt.imshow(output_image, origin='lower', extent=(-0.5, 1.5, -0.5, 1.5))
+    plt.grid(False)
+    return fig
+
+_ = visualize_classification(trained_model, dataset.data, dataset.label)
+plt.show()
