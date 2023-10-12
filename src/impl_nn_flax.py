9b555e150ba9a1ed5e1bb95fe16bac57e5ad9913
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-12 11:07:25 +0100

    Implemented dataloader class for the XOR dataset; up next Optimization

--- src/impl_nn_flax.py
+++ src/impl_nn_flax.py
@@ -95,3 +95,25 @@ def visualize_samples(data, label):
 
 visualize_samples(dataset.data, dataset.label)
 plt.show()
+
+
+# For the dataloader class
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple, list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+
+data_loader = data.DataLoader(dataset, batch_size=8, shuffle=True, collate_fn=numpy_collate)
+
+data_inputs, data_labels = next(iter(data_loader))
+
+print("Data inputs", data_inputs.shape, "\n", data_inputs)
+print("Data labels", data_labels.shape, "\n", data_labels)
+
+
+# Optimization
