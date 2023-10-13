f911a88803b0ac71aab842d5886009a6beedfede
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-13 01:33:05 +0100

    Setting up the train, val and test sets for the models

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -125,3 +125,127 @@ plt.show()
 
 
 # Analysing the effect of activation functions
+init_fn = lambda x: (lambda rng, shape, dtype: random.uniform(rng, shape=shape, minval=-1/np.sqrt(x.shape[1]), max_val=1/np.sqrt(x.shape[1]), dtype=dtype))
+
+
+# Network
+class BaseNetwork(nn.Module):
+    act_fn: nn.Module
+    num_classes: int = 10
+    hidden_sizes: Sequence = (512, 256, 256, 128)
+
+    @nn.compact
+    def __call__(self, x, return_activations=False):
+        x = x.reshape(x.shape[0], -1)
+        activations = []
+        for hd in self.hidden_sizes:
+            x = nn.Dense(hd, kernel_init=init_fn(x), bias_init=init_fn(x))(x)
+            activations.append(x)
+
+        x = nn.Dense(self.num_classes, kernel_init=init_fn(x), bias_init=init_fn(x))(x)
+        return x if not return_activations else (x, activations)
+
+
+def _get_config_file(model_path, model_name):
+    # Name of the file for storing hyperparameter details
+    return os.path.join(model_path, model_name + ".config")
+
+def _get_model_file(model_path, model_name):
+    # Name of the file for storing network parameters
+    return os.path.join(model_path, model_name + ".tar")
+
+def load_model(model_path, model_name, state=None):
+    """
+    Loads a saved model from disk.
+
+    Inputs:
+        model_path - Path of the checkpoint directory
+        model_name - Name of the model (str)
+        state - (Optional) If given, the parameters are loaded into this training state. Otherwise,
+                a new one is created alongside a network architecture.
+    """
+    config_file, model_file = _get_config_file(model_path, model_name), _get_model_file(model_path, model_name)
+    assert os.path.isfile(config_file), f"Could not find the config file \"{config_file}\". Are you sure this is the correct path and you have your model config stored here?"
+    assert os.path.isfile(model_file), f"Could not find the model file \"{model_file}\". Are you sure this is the correct path and you have your model stored here?"
+    with open(config_file, "r") as f:
+        config_dict = json.load(f)
+    if state is None:
+        act_fn_name = config_dict["act_fn"].pop("name").lower()
+        act_fn = act_fn_by_name[act_fn_name](**config_dict.pop("act_fn"))
+        net = BaseNetwork(act_fn=act_fn, **config_dict)
+        state = train_state.TrainState(step=0,
+                                       params=None,
+                                       apply_fn=net.apply,
+                                       tx=None,
+                                       opt_state=None)
+    else:
+        net = None
+    # You can also use flax's checkpoint package. To show an alternative,
+    # you can instead load the parameters simply from a pickle file.
+    with open(model_file, 'rb') as f:
+        params = pickle.load(f)
+    state = state.replace(params=params)
+    return state, net
+
+def save_model(model, params, model_path, model_name):
+    """
+    Given a model, we save the parameters and hyperparameters.
+
+    Inputs:
+        model - Network object without parameters
+        params - Parameters to save of the model
+        model_path - Path of the checkpoint directory
+        model_name - Name of the model (str)
+    """
+    config_dict = {
+        'num_classes': model.num_classes,
+        'hidden_sizes': model.hidden_sizes,
+        'act_fn': {'name': model.act_fn.__class__.__name__.lower()}
+    }
+    if hasattr(model.act_fn, 'alpha'):
+        config_dict['act_fn']['alpha'] = model.act_fn.alpha
+    os.makedirs(model_path, exist_ok=True)
+    config_file, model_file = _get_config_file(model_path, model_name), _get_model_file(model_path, model_name)
+    with open(config_file, "w") as f:
+        json.dump(config_dict, f)
+    # You can also use flax's checkpoint package. To show an alternative,
+    # you can instead save the parameters simply in a pickle file.
+    with open(model_file, 'wb') as f:
+        pickle.dump(params, f)
+
+
+import torch
+import torch.utils.data as data
+import torchvision
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+
+def image_to_numpy(img):
+    img = np.array(img, dtype=np.float32)
+    img = (img / 255. - 0.5) / 0.5
+    return img
+
+
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple, list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+
+train_dataset = FashionMNIST(root=DATASET_PATH, train=True, transform=image_to_numpy, download=True)
+
+train_set, val_set = torch.utils.data.random_split(train_dataset, [50000, 10000], generator=torch.Generator().manual_seed(42))
+
+test_set = FashionMNIST(root=DATASET_PATH, train=False, transform=image_to_numpy, download=True)
+
+train_loader = data.DataLoader(train_set, batch_size=1024, shuffle=False, drop_last=False, collate_fn=numpy_collate)
+
+val_loader = data.DataLoader(val_set, batch_size=1024, shuffle=False, drop_last=False, collate_fn=numpy_collate)
+test_loader = data.DataLoader(test_set, batch_size=1024, shuffle=False, drop_last=False, collate_fn=numpy_collate)
+
+# visualizing the gradient flow after initialization
+
