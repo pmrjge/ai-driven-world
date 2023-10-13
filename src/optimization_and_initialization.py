21848a773975123669dec9450e4c4f5c4b30c964
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-13 23:26:15 +0100

    Implemented the base network

--- src/optimization_and_initialization.py
+++ src/optimization_and_initialization.py
@@ -122,3 +122,123 @@ test_loader = data.DataLoader(test_set,
 
 print("Mean", (train_dataset.data.float() / 255.0).mean().item())
 print("Std", (train_dataset.data.float() / 255.0).std().item())
+
+imgs, _ = next(iter(train_loader))
+print(f"Mean: {imgs.mean().item():5.3f}")
+print(f"Standard deviation: {imgs.std().item():5.3f}")
+print(f"Maximum: {imgs.max().item():5.3f}")
+print(f"Minimum: {imgs.min().item():5.3f}")
+
+
+class BaseNetwork(nn.Module):
+    act_fn: Callable
+    num_classes: int = 10
+    hidden_sizes: Sequence = (512, 256, 256, 128)
+    kernel_init: Callable = nn.linear.default_kernel_init
+
+    @nn.compact
+    def __call__(self, x, return_activations=False):
+        x = x.reshape(x.shape[0], -1)
+        activations = []
+        for hd in self.hidden_sizes:
+            x = nn.Dense(hd, kernel_init=self.kernel_init)(x)
+            activations.append(x)
+            x = self.act_fn(x)
+            activations.append(x)
+        x = nn.Dense(self.num_classes, kernel_init=self.kernel_init)(x)
+        activations.append(x)
+        return x if not return_activations else (x, activations)
+
+
+act_fn_by_name = {
+    "tanh": nn.tanh,
+    "relu": nn.relu,
+    "identity": lambda x: x
+}
+
+
+##############################################################
+
+def plot_dists(val_dict, color="C0", xlabel=None, stat="count", use_kde=True):
+    columns = len(val_dict)
+    fig, ax = plt.subplots(1, columns, figsize=(columns*3, 2.5))
+    fig_index = 0
+    for key in sorted(val_dict.keys()):
+        key_ax = ax[fig_index%columns]
+        sns.histplot(val_dict[key], ax=key_ax, color=color, bins=50, stat=stat,
+                     kde=use_kde and ((val_dict[key].max()-val_dict[key].min())>1e-8)) # Only plot kde if there is variance
+        key_ax.set_title(f"{key} " + (r"(%i $\to$ %i)" % (val_dict[key].shape[1], val_dict[key].shape[0]) if len(val_dict[key].shape)>1 else ""))
+        if xlabel is not None:
+            key_ax.set_xlabel(xlabel)
+        fig_index += 1
+    fig.subplots_adjust(wspace=0.4)
+    return fig
+
+##############################################################
+
+def visualize_weight_distribution(params, color="C0"):
+    params, _ = jax.tree_util.tree_flatten(params)
+    params = [p.reshape(-1) for p in params if len(p.shape) > 1]  # Remove biases
+    params = jax.device_get(params)
+    weights = {f'Layer {layer_idx*2}': p for layer_idx, p in enumerate(params)}
+
+    ## Plotting
+    fig = plot_dists(weights, color=color, xlabel="Weight vals")
+    fig.suptitle("Weight distribution", fontsize=14, y=1.05)
+    plt.show()
+    plt.close()
+
+##############################################################
+
+small_loader = data.DataLoader(train_set, batch_size=1024, shuffle=False, collate_fn=numpy_collate)
+exmp_imgs, exmp_labels = next(iter(small_loader))
+
+def visualize_gradients(model, params, color="C0", print_variance=False):
+    """
+    Inputs:
+        net - Object of class BaseNetwork
+        color - Color in which we want to visualize the histogram (for easier separation of activation functions)
+    """
+    # Pass one batch through the network, and calculate the gradients for the weights
+    def loss_func(p):
+        logits = model.apply(p, exmp_imgs)
+        loss = optax.softmax_cross_entropy_with_integer_labels(logits, exmp_labels).mean()
+        return loss
+    grads = jax.grad(loss_func)(params)
+    grads = jax.device_get(grads)
+    # We limit our visualization to the weight parameters and exclude the bias to reduce the number of plots
+    grads = jax.tree_util.tree_leaves(grads)
+    grads = [g.reshape(-1) for g in grads if len(g.shape) > 1]
+    grads = {f'Layer {layer_idx*2}': g for layer_idx, g in enumerate(grads)}
+
+    ## Plotting
+    fig = plot_dists(grads, color=color, xlabel="Grad magnitude")
+    fig.suptitle("Gradient distribution", fontsize=14, y=1.05)
+    plt.show()
+    plt.close()
+
+    if print_variance:
+        for key in sorted(grads.keys()):
+            print(f"{key} - Variance: {np.var(grads[key])}")
+
+##############################################################
+
+def visualize_activations(model, params, color="C0", print_variance=False):
+    # Pass one batch through the network, and calculate the activations
+    _, activations = model.apply(params, exmp_imgs, return_activations=True)
+    activations = {f'Layer {layer_idx*2}': act.reshape(-1) for layer_idx, act in enumerate(activations[::2])}
+
+    ## Plotting
+    fig = plot_dists(activations, color=color, stat="density", xlabel="Activation vals")
+    fig.suptitle("Activation distribution", fontsize=14, y=1.05)
+    plt.show()
+    plt.close()
+
+    if print_variance:
+        for key in sorted(activations.keys()):
+            print(f"{key} - Variance: {np.var(activations[key])}")
+
+
+##############################################################
+
+# Initialization
