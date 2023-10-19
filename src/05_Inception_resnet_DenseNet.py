8c0f4eefb6359c0ecb82e6a0264e1f031e888b30
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-19 23:02:01 +0100

    Set train, val and test datasets

--- src/05_Inception_resnet_DenseNet.py
+++ src/05_Inception_resnet_DenseNet.py
@@ -74,3 +74,60 @@ DATA_MEANS = (train_dataset.data / 255.0).mean(axis=(0,1,2))
 DATA_STD = (train_dataset.data / 255.0).std(axis=(0,1,2))
 print("Data mean", DATA_MEANS)
 print("Data std", DATA_STD)
+
+# Transformations applied on each image => bring them into a numpy array
+def image_to_numpy(img):
+    img = np.array(img, dtype=np.float32)
+    img = (img / 255. - DATA_MEANS) / DATA_STD
+    return img
+
+# We need to stack the batch elements
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple,list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+
+test_transform = image_to_numpy
+# For training, we add some augmentation. Networks are too powerful and would overfit.
+train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                      transforms.RandomResizedCrop((32,32),scale=(0.8,1.0),ratio=(0.9,1.1)),
+                                      image_to_numpy
+                                     ])
+# Loading the training dataset. We need to split it into a training and validation part
+# We need to do a little trick because the validation set should not use the augmentation.
+train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
+val_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=test_transform, download=True)
+train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000], generator=torch.Generator().manual_seed(42))
+_, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000], generator=torch.Generator().manual_seed(42))
+
+# Loading the test set
+test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
+
+# We define a set of data loaders that we can use for training and validation
+train_loader = data.DataLoader(train_set,
+                               batch_size=128,
+                               shuffle=True,
+                               drop_last=True,
+                               collate_fn=numpy_collate,
+                               num_workers=8,
+                               persistent_workers=True)
+val_loader   = data.DataLoader(val_set,
+                               batch_size=128,
+                               shuffle=False,
+                               drop_last=False,
+                               collate_fn=numpy_collate,
+                               num_workers=4,
+                               persistent_workers=True)
+test_loader  = data.DataLoader(test_set,
+                               batch_size=128,
+                               shuffle=False,
+                               drop_last=False,
+                               collate_fn=numpy_collate,
+                               num_workers=4,
+                               persistent_workers=True)
+
