5e446918c0f5d4d112c66aca5f3efec118f03e66
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-19 23:13:42 +0100

    Implementation of the training module

--- src/05_Inception_resnet_DenseNet.py
+++ src/05_Inception_resnet_DenseNet.py
@@ -131,3 +131,169 @@ test_loader  = data.DataLoader(test_set,
                                num_workers=4,
                                persistent_workers=True)
 
+# trainer module
+
+class TrainState(train_state.TrainState):
+    batch_stats: Any
+
+
+class TrainerModule:
+    def __init__(self, model_name: str, model_class:nn.Module, model_hparams: dict, optimizer_name: str, optimizer_hparams: dict,
+                 exmp_imgs: Any, seed=42):
+        super().__init__()
+        self.model_name = model_name
+        self.model_class = model_class
+        self.model_hparams = model_hparams
+        self.optimizer_name = optimizer_name
+        self.optimizer_hparams = optimizer_hparams
+        self.seed = seed
+        self.model = self.model_class(**self.model_hparams)
+        self.log_dir = os.path.join(CHECKPOINT_PATH, self.model_name)
+        self.logger = SummaryWriter(log_dir=self.log_dir)
+        self.create_functions()
+        self.init_model(exmp_imgs)
+
+
+    def create_functions(self):
+        # Function to calculate the classification loss and accuracy for a model
+        def calculate_loss(params, batch_stats, batch, train):
+            imgs, labels = batch
+            # Run model. During training, we need to update the BatchNorm statistics.
+            outs = self.model.apply({'params': params, 'batch_stats': batch_stats},
+                                    imgs,
+                                    train=train,
+                                    mutable=['batch_stats'] if train else False)
+            logits, new_model_state = outs if train else (outs, None)
+            loss = optax.softmax_cross_entropy_with_integer_labels(logits, labels).mean()
+            acc = (logits.argmax(axis=-1) == labels).mean()
+            return loss, (acc, new_model_state)
+        # Training function
+        def train_step(state, batch):
+            loss_fn = lambda params: calculate_loss(params, state.batch_stats, batch, train=True)
+            # Get loss, gradients for loss, and other outputs of loss function
+            ret, grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+            loss, acc, new_model_state = ret[0], *ret[1]
+            # Update parameters and batch statistics
+            state = state.apply_gradients(grads=grads, batch_stats=new_model_state['batch_stats'])
+            return state, loss, acc
+        # Eval function
+        def eval_step(state, batch):
+            # Return the accuracy for a single batch
+            _, (acc, _) = calculate_loss(state.params, state.batch_stats, batch, train=False)
+            return acc
+        # jit for efficiency
+        self.train_step = jax.jit(train_step)
+        self.eval_step = jax.jit(eval_step)
+
+    def init_model(self, exmp_imgs):
+        # Initialize model
+        init_rng = jax.random.PRNGKey(self.seed)
+        variables = self.model.init(init_rng, exmp_imgs, train=True)
+        self.init_params, self.init_batch_stats = variables['params'], variables['batch_stats']
+        self.state = None
+
+    def init_optimizer(self, num_epochs, num_steps_per_epoch):
+        # Initialize learning rate schedule and optimizer
+        if self.optimizer_name.lower() == 'adam':
+            opt_class = optax.adam
+        elif self.optimizer_name.lower() == 'adamw':
+            opt_class = optax.adamw
+        elif self.optimizer_name.lower() == 'sgd':
+            opt_class = optax.sgd
+        else:
+            assert False, f'Unknown optimizer "{self.optimizer_name.lower()}"'
+        # We decrease the learning rate by a factor of 0.1 after 60% and 85% of the training
+        lr_schedule = optax.piecewise_constant_schedule(
+            init_value=self.optimizer_hparams.pop('lr'),
+            boundaries_and_scales=
+            {int(num_steps_per_epoch*num_epochs*0.6): 0.1,
+             int(num_steps_per_epoch*num_epochs*0.85): 0.1}
+        )
+        # Clip gradients at max value, and evt. apply weight decay
+        transf = [optax.clip(1.0)]
+        if opt_class == optax.sgd and 'weight_decay' in self.optimizer_hparams:  # wd is integrated in adamw
+            transf.append(optax.add_decayed_weights(self.optimizer_hparams.pop('weight_decay')))
+        optimizer = optax.chain(
+            *transf,
+            opt_class(lr_schedule, **self.optimizer_hparams)
+        )
+        # Initialize training state
+        self.state = TrainState.create(apply_fn=self.model.apply,
+                                       params=self.init_params if self.state is None else self.state.params,
+                                       batch_stats=self.init_batch_stats if self.state is None else self.state.batch_stats,
+                                       tx=optimizer)
+
+    def train_model(self, train_loader, val_loader, num_epochs=200):
+        # Train model for defined number of epochs
+        # We first need to create optimizer and the scheduler for the given number of epochs
+        self.init_optimizer(num_epochs, len(train_loader))
+        # Track best eval accuracy
+        best_eval = 0.0
+        for epoch_idx in tqdm(range(1, num_epochs+1)):
+            self.train_epoch(train_loader, epoch=epoch_idx)
+            if epoch_idx % 2 == 0:
+                eval_acc = self.eval_model(val_loader)
+                self.logger.add_scalar('val/acc', eval_acc, global_step=epoch_idx)
+                if eval_acc >= best_eval:
+                    best_eval = eval_acc
+                    self.save_model(step=epoch_idx)
+                self.logger.flush()
+
+    def train_epoch(self, train_loader, epoch):
+        # Train model for one epoch, and log avg loss and accuracy
+        metrics = defaultdict(list)
+        for batch in tqdm(train_loader, desc='Training', leave=False):
+            self.state, loss, acc = self.train_step(self.state, batch)
+            metrics['loss'].append(loss)
+            metrics['acc'].append(acc)
+        for key in metrics:
+            avg_val = np.stack(jax.device_get(metrics[key])).mean()
+            self.logger.add_scalar('train/'+key, avg_val, global_step=epoch)
+
+    def eval_model(self, data_loader):
+        # Test model on all images of a data loader and return avg loss
+        correct_class, count = 0, 0
+        for batch in data_loader:
+            acc = self.eval_step(self.state, batch)
+            correct_class += acc * batch[0].shape[0]
+            count += batch[0].shape[0]
+        eval_acc = (correct_class / count).item()
+        return eval_acc
+
+    def save_model(self, step=0):
+        # Save current model at certain training iteration
+        checkpoints.save_checkpoint(ckpt_dir=self.log_dir,
+                                    target={'params': self.state.params,
+                                            'batch_stats': self.state.batch_stats},
+                                    step=step,
+                                    overwrite=True)
+
+    def load_model(self, pretrained=False):
+        # Load model. We use different checkpoint for pretrained models
+        if not pretrained:
+            state_dict = checkpoints.restore_checkpoint(ckpt_dir=self.log_dir, target=None)
+        else:
+            state_dict = checkpoints.restore_checkpoint(ckpt_dir=os.path.join(CHECKPOINT_PATH, f'{self.model_name}.ckpt'), target=None)
+        self.state = TrainState.create(apply_fn=self.model.apply,
+                                       params=state_dict['params'],
+                                       batch_stats=state_dict['batch_stats'],
+                                       tx=self.state.tx if self.state else optax.sgd(0.1)   # Default optimizer
+                                       )
+
+    def checkpoint_exists(self):
+        # Check whether a pretrained model exist for this autoencoder
+        return os.path.isfile(os.path.join(CHECKPOINT_PATH, f'{self.model_name}.ckpt'))
+
+
+def train_classifier(*args, num_epochs=200, **kwargs):
+    # Create a trainer module with specified hyperparameters
+    trainer = TrainerModule(*args, **kwargs)
+    if not trainer.checkpoint_exists():  # Skip training if pretrained model exists
+        trainer.train_model(train_loader, val_loader, num_epochs=num_epochs)
+        trainer.load_model()
+    else:
+        trainer.load_model(pretrained=True)
+    # Test trained model
+    val_acc = trainer.eval_model(val_loader)
+    test_acc = trainer.eval_model(test_loader)
+    return trainer, {'val': val_acc, 'test': test_acc}
